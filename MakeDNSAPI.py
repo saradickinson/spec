d430b234614dfd9a663846bc18f7b21d86d0bb3c
Willem Toorop <willem@nlnetlabs.nl>
2013-11-13 13:36:17 +0000

    Remove redundant getdns_free_sync_request_memory function. Use getdns_dict_destroy instead.
    version 356

--- MakeDNSAPI.py
+++ MakeDNSAPI.py
@@ -454,7 +454,7 @@ APIdesc = "index.html"
 APIcoreName = "getdns_core_only"
 APItemplate = "APItemplate.html"
 BackupDir = "NotForSVN/Backups"
-VersionNumber = "0.355"
+VersionNumber = "0.356"
 ThisTarballName = "getdns-" + VersionNumber + ".tgz"
 TheExamplesToMake = [ 
 	"example-all-functions",
