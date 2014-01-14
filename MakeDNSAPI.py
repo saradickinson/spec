cd8469a42a9a1b8d62ea65359300577fafa9e9d1
Willem Toorop <willem@nlnetlabs.nl>
2014-01-14 14:17:23 +0000

    Add const to appropriate function parameters
    API version 0.375

--- MakeDNSAPI.py
+++ MakeDNSAPI.py
@@ -458,7 +458,7 @@ APIdesc = "index.html"
 APIcoreName = "getdns_core_only"
 APItemplate = "APItemplate.html"
 BackupDir = "NotForSVN/Backups"
-VersionNumber = "0.374"
+VersionNumber = "0.375"
 ThisTarballName = "getdns-" + VersionNumber + ".tgz"
 TheExamplesToMake = [ 
 	"example-all-functions",
