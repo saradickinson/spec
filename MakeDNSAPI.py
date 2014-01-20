fa0340d44fc68d1a878ead3d5edf66a3a8c8b351
Willem Toorop <willem@nlnetlabs.nl>
2014-01-20 14:44:17 +0000

    Renamed getdns_context_set_stub_resolution to getdns_context_set_upstream_recursive_servers.
    API version 0.376

--- MakeDNSAPI.py
+++ MakeDNSAPI.py
@@ -458,7 +458,7 @@ APIdesc = "index.html"
 APIcoreName = "getdns_core_only"
 APItemplate = "APItemplate.html"
 BackupDir = "NotForSVN/Backups"
-VersionNumber = "0.375"
+VersionNumber = "0.376"
 ThisTarballName = "getdns-" + VersionNumber + ".tgz"
 TheExamplesToMake = [ 
 	"example-all-functions",
