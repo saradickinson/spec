4a2eaa6bbc320e00a7c04e287c3f8c2f60a1a5cc
Willem Toorop <willem@nlnetlabs.nl>
2014-02-03 20:11:12 +0000

    Bugfix replace < and > with &lt; and &gt; html entities
    API version 0.381

--- MakeDNSAPI.py
+++ MakeDNSAPI.py
@@ -458,7 +458,7 @@ APIdesc = "index.html"
 APIcoreName = "getdns_core_only"
 APItemplate = "APItemplate.html"
 BackupDir = "NotForSVN/Backups"
-VersionNumber = "0.380"
+VersionNumber = "0.381"
 ThisTarballName = "getdns-" + VersionNumber + ".tgz"
 TheExamplesToMake = [ 
 	"example-all-functions",
