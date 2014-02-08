a482d7401b27329847396886adf8e33958dbd65b
Willem Toorop <willem@nlnetlabs.nl>
2014-02-08 15:01:15 +0000

    Millisecond timeouts as uint64_t. Fix DNSSEC skew bug (can only be seconds).
    API version 0.389

--- MakeDNSAPI.py
+++ MakeDNSAPI.py
@@ -472,7 +472,7 @@ APIdesc = "index.html"
 APIcoreName = "getdns_core_only"
 APItemplate = "APItemplate.html"
 BackupDir = "NotForSVN/Backups"
-VersionNumber = "0.388"
+VersionNumber = "0.389"
 ThisTarballName = "getdns-" + VersionNumber + ".tgz"
 TheExamplesToMake = [ 
 	"example-all-functions",
