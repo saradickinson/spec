1a7a9e14a302cc7a6046eb4f744fc87638375d6b
Willem Toorop <willem@nlnetlabs.nl>
2014-02-08 15:05:17 +0000

    API version update + fix another example code dereference bug

--- MakeDNSAPI.py
+++ MakeDNSAPI.py
@@ -472,7 +472,7 @@ APIdesc = "index.html"
 APIcoreName = "getdns_core_only"
 APItemplate = "APItemplate.html"
 BackupDir = "NotForSVN/Backups"
-VersionNumber = "0.389"
+VersionNumber = "0.390"
 ThisTarballName = "getdns-" + VersionNumber + ".tgz"
 TheExamplesToMake = [ 
 	"example-all-functions",
